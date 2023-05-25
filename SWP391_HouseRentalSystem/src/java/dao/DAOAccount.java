d2f0414506fa7c202d210f55ef0ba04f71f89abe
hieunthe163894 <hieunthe163894@fpt.edu.vn>
2023-05-25 23:37:55 +0700

    add

--- SWP391_HouseRentalSystem/src/java/dao/DAOAccount.java
+++ SWP391_HouseRentalSystem/src/java/dao/DAOAccount.java
@@ -4,6 +4,7 @@
  */
 package dao;
 
+import context.DBContext;
 import entity.Account;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -19,80 +20,80 @@ import model.DBConnect;
  * @author win
  */
 public class DAOAccount extends DBConnect {
-    
-    public int addAccount(Account acc) {
-        int n = 0;
-        String sql = "INSERT INTO [dbo].[Account]\n"
-                + "           ([Fullname]\n"
-                + "           ,[Gender]\n"
-                + "           ,[Address]\n"
-                + "           ,[Email]\n"
-                + "           ,[Password]\n"
-                + "           ,[Phone_Number]\n"
-                + "           ,[Role_ID]\n"
-                + "           ,[Status])\n"
-                + "           ,[Secure_Question_ID])\n"
-                + "           ,[Secure_Answer_ID])\n"
-                + "           ,[Profile_Picture])\n"
-                + "     VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-        try {
-            PreparedStatement pre = conn.prepareStatement(sql);
-            // set parameter ?
-            pre.setString(1, acc.getFullname());
-            pre.setBoolean(2, acc.isGender());
-            pre.setString(3, acc.getAddress());
-            pre.setString(4, acc.getEmail());
-            pre.setString(5, acc.getPassword());
-            pre.setString(6, acc.getPhone_Number());
-            pre.setInt(7, acc.getRole_ID());
-            pre.setInt(8, acc.getStatus());
-            pre.setInt(9, acc.getSecure_Question_ID());
-            pre.setInt(10, acc.getSecure_Answer_ID());
-            pre.setString(11, acc.getProfile_Picture());
-            // run
-            n = pre.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(DAOAccount.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return n;
-    }
-    
-    public int updateAccount(Account acc) {
-        int n = 0;
-        String sql = "UPDATE [dbo].[Account]\n"
-                + "   SET [Fullname] = ?\n"
-                + "      ,[Gender] = ?\n"
-                + "      ,[Address] = ?\n"
-                + "      ,[Password] = ?\n"
-                + "      ,[Phone_Number] = ?\n"
-                + "      ,[Role_ID] = ?\n"
-                + "      ,[Status] = ?\n"
-                + "      ,[Secure_Question_ID] = ?\n"
-                + "      ,[Secure_Answer_ID] = ?\n"
-                + "      ,[Profile_Picture] = ?\n"
-                + " WHERE [Email] = ?";
-        
-        try {
-            PreparedStatement pre = conn.prepareStatement(sql);
-            pre.setString(1, acc.getFullname());
-            pre.setBoolean(2, acc.isGender());
-            pre.setString(3, acc.getAddress());
-            pre.setString(4, acc.getEmail());
-            pre.setString(5, acc.getPassword());
-            pre.setString(6, acc.getPhone_Number());
-            pre.setInt(7, acc.getRole_ID());
-            pre.setInt(8, acc.getStatus());
-            pre.setInt(9, acc.getSecure_Question_ID());
-            pre.setInt(10, acc.getSecure_Answer_ID());
-            pre.setString(11, acc.getProfile_Picture());
-            // RUN
-            n = pre.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(DAOAccount.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return n;
-    }
-    
+
+//    public int addAccount(Account acc) {
+//        int n = 0;
+//        String sql = "INSERT INTO [dbo].[Account]\n"
+//                + "           ([Fullname]\n"
+//                + "           ,[Gender]\n"
+//                + "           ,[Address]\n"
+//                + "           ,[Email]\n"
+//                + "           ,[Password]\n"
+//                + "           ,[Phone_Number]\n"
+//                + "           ,[Role_ID]\n"
+//                + "           ,[Status])\n"
+//                + "           ,[Secure_Question_ID])\n"
+//                + "           ,[Secure_Answer_ID])\n"
+//                + "           ,[Profile_Picture])\n"
+//                + "     VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+//        try {
+//            PreparedStatement pre = conn.prepareStatement(sql);
+//            // set parameter ?
+//            pre.setString(1, acc.getFullname());
+//            pre.setBoolean(2, acc.isGender());
+//            pre.setString(3, acc.getAddress());
+//            pre.setString(4, acc.getEmail());
+//            pre.setString(5, acc.getPassword());
+//            pre.setString(6, acc.getPhone_Number());
+//            pre.setInt(7, acc.getRole_ID());
+//            pre.setInt(8, acc.getStatus());
+//            pre.setInt(9, acc.getSecure_Question_ID());
+//            pre.setInt(10, acc.getSecure_Answer_ID());
+//            pre.setString(11, acc.getProfile_Picture());
+//            // run
+//            n = pre.executeUpdate();
+//        } catch (SQLException ex) {
+//            Logger.getLogger(DAOAccount.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return n;
+//    }
+
+//    public int updateAccount(Account acc) {
+//        int n = 0;
+//        String sql = "UPDATE [dbo].[Account]\n"
+//                + "   SET [Fullname] = ?\n"
+//                + "      ,[Gender] = ?\n"
+//                + "      ,[Address] = ?\n"
+//                + "      ,[Password] = ?\n"
+//                + "      ,[Phone_Number] = ?\n"
+//                + "      ,[Role_ID] = ?\n"
+//                + "      ,[Status] = ?\n"
+//                + "      ,[Secure_Question_ID] = ?\n"
+//                + "      ,[Secure_Answer_ID] = ?\n"
+//                + "      ,[Profile_Picture] = ?\n"
+//                + " WHERE [Email] = ?";
+//
+//        try {
+//            PreparedStatement pre = conn.prepareStatement(sql);
+//            pre.setString(1, acc.getFullname());
+//            pre.setBoolean(2, acc.isGender());
+//            pre.setString(3, acc.getAddress());
+//            pre.setString(4, acc.getEmail());
+//            pre.setString(5, acc.getPassword());
+//            pre.setString(6, acc.getPhone_Number());
+//            pre.setInt(7, acc.getRole_ID());
+//            pre.setInt(8, acc.getStatus());
+//            pre.setInt(9, acc.getSecure_Question_ID());
+//            pre.setInt(10, acc.getSecure_Answer_ID());
+//            pre.setString(11, acc.getProfile_Picture());
+//            // RUN
+//            n = pre.executeUpdate();
+//        } catch (SQLException ex) {
+//            Logger.getLogger(DAOAccount.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return n;
+//    }
+
     public Vector<Account> getAll() {
         Vector<Account> vector = new Vector<Account>();
         String sql = "select * from Account";
@@ -120,7 +121,7 @@ public class DAOAccount extends DBConnect {
         }
         return vector;
     }
-    
+
     public Vector<Account> getAll(String sql) {
         Vector<Account> vector = new Vector<Account>();
         // String sql = "select * from Account";
@@ -148,7 +149,7 @@ public class DAOAccount extends DBConnect {
         }
         return vector;
     }
-    
+
     public int removeAccount(int email) {
         int n = 0;
         String sql = "delete from Account where Email=" + email;
@@ -165,7 +166,7 @@ public class DAOAccount extends DBConnect {
         }
         return n;
     }
-    
+
     public Account Login(String email, String password) {
         String sql = "SELECT * FROM Account WHERE Email = ? and Password = ?";
         try {
@@ -173,7 +174,7 @@ public class DAOAccount extends DBConnect {
             pre.setString(1, email);
             pre.setString(2, password);
             ResultSet rs = pre.executeQuery();
-            while (rs.next()) {                
+            while (rs.next()) {
                 return new Account(rs.getInt(1),
                         rs.getString(2),
                         rs.getString(3),
@@ -191,12 +192,44 @@ public class DAOAccount extends DBConnect {
             Logger.getLogger(DAOAccount.class.getName()).log(Level.SEVERE, null, e);
         }
         return null;
-    }    
-    
+    }
+
+    public void signup(String fullname, int gender, String adress, String email, String password, String phonenumbers, int roleid, int status, int squestion, int aquestion, String img) {
+        String sql = "INSERT INTO [dbo].[Account]\n"
+                + "           ([Fullname]\n"
+                + "           ,[Gender]\n"
+                + "           ,[Address]\n"
+                + "           ,[Email]\n"
+                + "           ,[Password]\n"
+                + "           ,[Phone_Number]\n"
+                + "           ,[Role_ID]\n"
+                + "           ,[Status]\n"
+                + "           ,[Secure_Question_ID]\n"
+                + "           ,[Secure_Answer_ID]\n"
+                + "           ,[Profile_Picture])\n"
+                + "     VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, fullname);
+            ps.setInt(2, gender);
+            ps.setString(3, adress);
+            ps.setString(4, email);
+            ps.setString(5, password);
+            ps.setString(6, phonenumbers);
+            ps.setInt(7, roleid);
+            ps.setInt(8, status);
+            ps.setInt(9, squestion);
+            ps.setInt(10, aquestion);
+            ps.setString(11, img);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
     public static void main(String[] args) {
         DAOAccount dao = new DAOAccount();
-        Account acc = dao.Login("TrungHieu@gmail.com", "123456");
-        System.out.println(acc);
+        dao.signup("Thang", 1, "Ha Noi", "thang@gmail.com", "abc123", "9999999999", 1, 1, 1, 1, "NULL");
+        
     }
     ////test
 }
--- SWP391_HouseRentalSystem/web/home.jsp
+++ SWP391_HouseRentalSystem/web/home.jsp
@@ -60,7 +60,7 @@
                             <!-- Nav Start -->
                             <div class="classynav">
                                 <ul>
-                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="home">Home</a></li>
                                     <li><a href="#">Pages</a>
                                         <ul class="dropdown">
                                             <li><a href="index.html">Home</a></li>
