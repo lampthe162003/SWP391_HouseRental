3cc95dbfba4564dcf714e392abd1326098062893
lampthe162003 <lampthe162003@fpt.edu.vn>
2023-06-05 10:48:38 +0700

    Add new DAO for house and add insert method

---
+++ SWP391_HouseRentalSystem/src/java/dao/DAOHouse.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import context.DBContext;
+import entity.House;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Hayashi
+ */
+public class DAOHouse {
+
+    public void insert(House house) {
+        try {
+            Connection connection = new DBContext().getConnection();
+            String sql = "INSERT INTO House([Id]\n"
+                    + "      ,[House_Owner_ID]\n"
+                    + "      ,[Category_ID]\n"
+                    + "      ,[Price]\n"
+                    + "      ,[District_ID]\n"
+                    + "      ,[Full_Address]\n"
+                    + "      ,[Description]\n"
+                    + "      ,[Rating]\n"
+                    + "      ,[Added_Date])"
+                    + "VALUES"
+                    + "(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, house.getId());
+            stm.setInt(2, house.getHouse_Owener_ID());
+            stm.setInt(3, house.getCategory_ID());
+            stm.setString(4, house.getPrice());
+            stm.setInt(5, house.getDistrict_ID());
+            stm.setString(6, house.getFull_Adress());
+            stm.setString(7, house.getText());
+            stm.setFloat(8, house.getRating());
+            stm.setDate(9, house.getAdded_Date());
+            stm.executeUpdate();
+            connection.commit();
+        } catch (Exception ex) {
+            Logger.getLogger(DAOHouse.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+}
