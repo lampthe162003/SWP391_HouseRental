32f2bd36b1c862d7c120995f24633cbe77fe49bd
luonbenem2002 <luonbenem2002@gmail.com>
2023-06-12 13:01:32 +0700

    cmt

--- SWP391_HouseRentalSystem/src/java/dao/DAOHouse.java
+++ SWP391_HouseRentalSystem/src/java/dao/DAOHouse.java
@@ -5,17 +5,22 @@
 package dao;
 
 import context.DBContext;
+import entity.House_Images;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  *
  * @author ADMIN
  */
 public class DAOHouse {
-    public void insertHouse(int houseOwnerId, int categoryId, String price, int districtId, String address, String description,String title){
+
+    public void insertHouse(int houseOwnerId, int categoryId, String price, int districtId, String address, String description, String title) {
         try {
             String stmSql = "insert into House(House_Owner_ID,Category_ID,Price,District_ID,Full_Address,Description,Rating,Added_Date,Title) values(?,?,?,?,?,?,?,?,?)";
             Date now = new Date();
@@ -38,5 +43,67 @@ public class DAOHouse {
             System.out.println(e.getMessage());
         }
     }
-    
+
+    public int getHouseId() {
+        try {
+            String stmSql = "select top(1) Id from House order by Id desc";
+            Connection conn = new DBContext().getConnection();
+            PreparedStatement ps = conn.prepareStatement(stmSql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return 0;
+    }
+
+    public void insertHouseDetail(int houseId, int nBedroom, int nBathroom, int area, int nPool, int houseDirectionId) {
+        try {
+            String stmSql = "insert into House_Details(House_ID,Number_Of_Bedrooms,Number_Of_Bathrooms,Area,Pool,House_Direction_ID) values (?,?,?,?,?,?)";
+            Connection conn = new DBContext().getConnection();
+            PreparedStatement ps = conn.prepareStatement(stmSql);
+            ps.setInt(1, houseId);
+            ps.setInt(2, nBedroom);
+            ps.setInt(3, nBathroom);
+            ps.setInt(4, area);
+            ps.setInt(5, nPool);
+            ps.setInt(6, houseDirectionId);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void insertImages(int houseId, String image) {
+        try {
+            String stmSql = "insert into House_Images(House_ID,Image) values (?,?)";
+            Connection conn = new DBContext().getConnection();
+            PreparedStatement ps = conn.prepareStatement(stmSql);
+            ps.setInt(1, houseId);
+            ps.setString(2, image);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public List<House_Images> getListHouseImageByHouseId(int houseId){
+        try {
+            List<House_Images> lsHouseImage = new ArrayList<>();
+            String stmSql = "select * from House_Images where House_ID = ?";
+            Connection conn = new DBContext().getConnection();
+            PreparedStatement ps = conn.prepareStatement(stmSql);
+            ps.setInt(1, houseId);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                House_Images hi = new House_Images(rs.getInt(1), rs.getInt(2), rs.getString(3));
+                lsHouseImage.add(hi);
+            }
+            return lsHouseImage;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
