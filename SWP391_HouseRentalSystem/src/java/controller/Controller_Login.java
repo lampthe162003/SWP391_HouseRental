c179f594ddf98a1f3a032f5da31058fc9440e29f
luonbenem2002 <luonbenem2002@gmail.com>
2023-06-19 13:24:19 +0700

    cmt

--- SWP391_HouseRentalSystem/src/java/controller/Controller_Login.java
+++ SWP391_HouseRentalSystem/src/java/controller/Controller_Login.java
@@ -1,4 +1,3 @@
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
@@ -6,20 +5,14 @@
 package controller;
 
 import dao.DAOAccount;
-import dao.DAOCategory;
-import dao.DAODistricts;
-import dao.DAOHouse;
 import entity.Account;
-import entity.Districts;
-import entity.House;
-import entity.House_Category;
 import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import java.util.List;
 
 /**
  *
@@ -36,6 +29,7 @@ public class Controller_Login extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -69,38 +63,16 @@ public class Controller_Login extends HttpServlet {
         DAOAccount dao = new DAOAccount();
         Account acc = dao.Login(email, password);
         if (acc == null) {
-            request.setAttribute("mess", "Wrong user or password");
-            request.getRequestDispatcher("login.jsp").forward(request, response);
+            response.getWriter().println("Login k duoc");
         } else {
             HttpSession session = request.getSession();
             session.setAttribute("acc", acc);
-            if (acc.getRole_ID() == 1) {
-                response.sendRedirect("admin-home");
-            } else {
-                request.getSession().setAttribute("searchSession", 1);
-                DAOCategory daoc = new DAOCategory();
-                DAODistricts daod = new DAODistricts();
-                DAOHouse daoh = new DAOHouse();
-
-                int index = 1;
-
-                List<House> listh = daoh.getListHouse(index);
-                List<House_Category> listc = daoc.getListCategory();
-                List<Districts> litsd = daod.getListDistricts();
-
-                int count = daoh.totalHouse();
-                int size = 3;
-                int endPage = count / size;
-                if (count % size != 0) {
-                    endPage++;
-                }
-
-                request.setAttribute("endPage", endPage);
-                request.setAttribute("list_house", listh);
-                request.setAttribute("list_category", listc);
-                request.setAttribute("list_districts", litsd);
-                request.getRequestDispatcher("home1.jsp").forward(request, response);
-            }
+            response.sendRedirect("home");
         }
     }
 }
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
