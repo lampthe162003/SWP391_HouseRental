d64bdcfe55567b2312bf7413095c5c0ac35499a7
hieunthe163894 <hieunthe163894@fpt.edu.vn>
2023-06-22 14:51:03 +0700

    fix

--- SWP391_HouseRentalSystem/src/java/controller/Controller_Change_Information.java
+++ SWP391_HouseRentalSystem/src/java/controller/Controller_Change_Information.java
@@ -47,7 +47,6 @@ public class Controller_Change_Information extends HttpServlet {
             out.println("</html>");
         }
     }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
