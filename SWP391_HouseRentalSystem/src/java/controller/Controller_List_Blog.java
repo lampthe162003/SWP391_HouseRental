9e2113fa5ff4e609addafc7d0eb8b38ea9c657cc
luonbenem2002 <luonbenem2002@gmail.com>
2023-06-29 15:18:04 +0700

    cmt

--- SWP391_HouseRentalSystem/src/java/controller/Controller_List_Blog.java
+++ SWP391_HouseRentalSystem/src/java/controller/Controller_List_Blog.java
@@ -71,10 +71,11 @@ public class Controller_List_Blog extends HttpServlet {
             if(endPage==0){
                 endPage = 1;
             }
-            else if ((countBlog / size) != 0) {
+            else if ((countBlog % size) != 0) {
                 endPage++;
             }
             List<Blog> lsB = blog.getListBlog(index * size - 2, index * size);
+            request.setAttribute("countB", countBlog);
             request.setAttribute("lsB", lsB);
             request.setAttribute("endPage", endPage);
             request.setAttribute("index", index);
@@ -87,13 +88,16 @@ public class Controller_List_Blog extends HttpServlet {
             if(endPage==0){
                 endPage = 1;
             }
-            else if ((countBlog / size) != 0) {
+            else if ((countBlog % size) != 0) {
                 endPage++;
             }
             List<Blog> lsB = blog.getListBlog(index * size - 2, index * size);
             request.setAttribute("lsB", lsB);
             request.setAttribute("endPage", endPage);
             request.setAttribute("index", index);
+        }
+        if(a!=null){
+            request.setAttribute("idA", a.getId());
         }
         request.getRequestDispatcher("single-blog.jsp").forward(request, response);
     }
